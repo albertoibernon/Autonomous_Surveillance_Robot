9d1c4b022916b81e666319043f3ee13fe094488c
Josep Mª Barberá Civera <chemabc@gmail.com>
2023-12-19 13:33:39 +0100

    Update generateMap.m

--- generateMap.m
+++ generateMap.m
@@ -1,9 +1,10 @@
-function [map]  = generateMap(path, threshold, scale)
+function [map]  = generateMap(path, threshold, scale, side)
 
 
 image = imread(path);
 %Convert image into grayscale.
 grayimage = rgb2gray(image);
+grayimage = imresize(grayimage, [side*scale, side*scale]);
 % imshow(grayimage);
 
 % Threshold  image in order to get a binary image.
